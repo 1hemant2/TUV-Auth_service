6a673487b86c3fd057c920737b1fa45f205274e6
hemant <hk443957@gmail.com>
2025-02-27 01:39:33 +0530

    make _id primary key, added a task lists link in notes.js

--- query/createTable.js
+++ query/createTable.js
@@ -1,7 +1,7 @@
 const createTable = {
    create_user_table :
       `create table users (
-            _id uuid,
+            _id uuid primary key,
             name varchar(200),
             username varchar(200) unique,
             email varchar(200) unique,
@@ -13,7 +13,7 @@ const createTable = {
         )`,
    create_OTP_table : 
        `create table otp (
-            _id uuid,
+            _id uuid primary key,
             user_id uuid,
             otp TEXT unique,
             timestamp TIMESTAMPTZ DEFAULT NOW()
@@ -21,7 +21,7 @@ const createTable = {
         )`,
     create_access_token_table : 
        `create table access_token (
-            _id uuid,
+            _id uuid primary key,
             user_id uuid,
             access_token TEXT,
             timestamp TIMESTAMPTZ DEFAULT NOW()
@@ -37,7 +37,7 @@ const createTable = {
         )`,
     create_logs_table : 
         `create table logs (
-             _id uuid,
+            _id uuid primary key,
             name varchar(50),
             username varchar(50),
             email varchar(200),
@@ -51,7 +51,7 @@ const createTable = {
         )`,
     create_role_table : 
         `create table role (
-            _id uuid, 
+            _id uuid primary key,
             user_id uuid,
             name TEXT,
             timestamp TIMESTAMPTZ DEFAULT NOW()
@@ -59,7 +59,7 @@ const createTable = {
         )`,
     create_allowed_role_table : 
         `create table allowed_roles(
-            _id uuid, 
+            _id uuid primary key,
             name TEXT, 
             timestamp TIMESTAMPTZ DEFAULT NOW()
             createdAT TIMESTAMPTZ DEFAULT NOW()
